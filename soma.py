9012dd12245730ff708d9037c16f2e0cc85bafd0
lampiao <sandrogsfilho@gmail.com>
2024-11-06 18:26:24 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,12 @@
+# Funções
+# Função soma
+def soma(a, b):
+    return a + b
+
+
+valorA = int(input("Digite um valor: "))
+valorB = int(input("Digite um valor: "))
+
+resultado = soma(valorA, valorB)
+
+print(f"O resultado da soma = {resultado}")
