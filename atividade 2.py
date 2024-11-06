f00f396fa1321bd8964e2a4d212abdea2104de54
lampiao <sandrogsfilho@gmail.com>
2024-11-06 18:19:27 -0300

    Create atividade 2.py

---
+++ atividade 2.py
@@ -0,0 +1,10 @@
+#Crie um algoritmo que armazene dois valores distintos: A e B. 
+# Depois efetue a troca dos valores de forma que a variável A passe a possuir o valor de B e virce versa. 
+# Após, apresente os valores trocados. (A=5 | B-10)
+a=5
+b=10
+print("Os valores de A e B são respectivamente", a, "e", b)
+c=a
+a=b
+b=c
+print("Agora Ae B valem", a, "e", b)
