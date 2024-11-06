6fc17768f3c62da93809400dc5a791d7eb1bae84
lampiao <sandrogsfilho@gmail.com>
2024-11-06 18:18:47 -0300

    Create atividade1.py

---
+++ atividade1.py
@@ -0,0 +1,12 @@
+#Algoritmo média: criar uma algoritmo que leia 4 notas e apresente uma média final
+
+print("Olá! Vamos ver qual será sua média.")
+
+notal = float(input ("Digite o valor de da sua primeira nota: "))
+nota2 = float(input ("Digite o valor de da sua segunda nota: "))
+nota3= float(input ("Digite o valor de da sua terceira nota: "))
+nota4 = float(input ("Digite o valor de da sua quata nota: "))
+
+media = (notal + nota2 + nota3 + nota4)/4
+
+print("Sua média é", media)
