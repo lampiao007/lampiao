f7e87056345a0fd77612354a6fbee80e956fb800
lampiao <sandrogsfilho@gmail.com>
2024-11-06 18:17:58 -0300

    Create  comandos.py

---
+++ comandos.py
@@ -0,0 +1,22 @@
+# Comenários
+
+# Tipagem dinâmica
+# teste = "olá"
+# print(type(teste))
+# teste = "30"
+# print(type(teste))
+# teste = "2.4"
+# print(type(teste))
+
+# nome = input("Digite o seu nome: ")
+#print("ola", nome)
+
+#Operadores aritméticos
+x=20
+print(x+4) #adição
+print(x-7) #subtração
+print(x*2) #multiplicação
+print(x/3) #divisão
+print(x//6) #divisão inteira
+print(x**1) #potenciação
+print(x%5) #resto da divisão
